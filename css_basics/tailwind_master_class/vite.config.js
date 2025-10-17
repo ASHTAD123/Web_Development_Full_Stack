763e31d9564d39393f403f9e6b3e0df90a84d740
ASHTAD IRANI <ashtadcyrusirani@gmail.com>
2025-10-17 19:44:14 +0530

    happy-diwali-tailwind css example in tailwind-mater-class

--- css_basics/tailwind_master_class/vite.config.js
+++ css_basics/tailwind_master_class/vite.config.js
@@ -5,4 +5,17 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  theme: {
+    extend: {
+      keyframes: {
+        bend: {
+          "0%, 100%": { transform: "skewY(0deg)" },
+          "50%": { transform: "skewY(10deg)" },
+        },
+      },
+      animation: {
+        bend: "bend 1.5s ease-in-out infinite",
+      },
+    },
+  },
 });
