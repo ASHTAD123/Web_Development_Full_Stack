d7a9c421bd68638174b3f6117eb64430ddf28f27
ASHTAD IRANI <ashtadcyrusirani@gmail.com>
2025-10-18 21:19:46 +0530

    data types javascript

---
+++ javascript/Part_1/01_Datatypes_variables.js
@@ -0,0 +1,21 @@
+// String , Number , Boolean , BigInt, Undefined , null , Object , Symbol
+
+//Number
+let score = 102;
+
+//String
+let name="Ash"
+
+//Boolean
+let isLoggedIn = false;
+
+//Array
+let typesOfGames = ["GTA_5","Detriot became human","Alien Isolation","Halo"]
+
+//Object
+let userProfile = [{name:'Ash',age:25,city:'Mumbai'}]
+
+const a =20;
+// a=39; A constant cannot be changed
+
+console.log(a);
